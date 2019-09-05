c4a1e22045222aee5c03904c7ac66cb9e8f94ccf
sernia <sernia@gmail.com>
2019-09-05 16:54:37 +0900

    webサイトを参照

--- controller/mecab.py
+++ controller/mecab.py
@@ -1,8 +1,10 @@
 import MeCab
 import sys
 import re
+import urllib
 from flask import Flask
 from flask import render_template
+from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 def mecab_result():
@@ -19,10 +21,19 @@ def mecab_result():
     while node:
         meta = node.feature.split(",")
         if meta[0] == '名詞':
+            #スクレイピング
+            scriping(node.surface)
+
             keywords.append(node.surface + "," + meta[7])
             #keywords.append(node.text)
         node = node.next
-
-        #CSV作成
-
     return keywords
+
+def scriping(txt):
+    convertTxt = urllib.parse.quote_plus(txt, encoding='utf-8')
+    url = "https://www.kpedia.jp/s/1/{}".format(convertTxt)
+    html = urllib.request.urlopen(url=url)
+    soup = BeautifulSoup(html, "html.parser")
+    tableTag = soup.table
+    table = tableTag.string
+    print(table)
