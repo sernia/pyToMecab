30da2fb5bf48f2d4d25184738314f8660cce1de6
sernia <sernia@gmail.com>
2019-09-06 15:50:25 +0900

    詳細を登録する

--- controller/mecab.py
+++ controller/mecab.py
@@ -34,6 +34,35 @@ def scriping(txt):
     url = "https://www.kpedia.jp/s/1/{}".format(convertTxt)
     html = urllib.request.urlopen(url=url)
     soup = BeautifulSoup(html, "html.parser")
-    tableTag = soup.table
-    table = tableTag.string
-    print(table)
+
+    #単語テーブルを取得
+    wordTable = soup.findAll("table", {"class": "school-course"})
+    #タグの存在を確認
+    if len(wordTable) == 0:
+        return 0
+
+    #タグ確認後、aタグ一覧を取得    
+    trTag = wordTable[0].findAll("tr")
+
+    word = "none"
+    for trRow in trTag:
+        item = trRow.findAll("td")
+        #タグの存在を確認
+        if len(item) == 0:
+            continue
+        if txt == item[1].get_text():
+            word = item[0].get_text().split('（')[0]
+            break
+
+    #例テーブルを取得
+    listTable = soup.findAll("table", {"style":"font-size:14px;line-height:20px;margin-bottom:20px;"})
+    tdTag = listTable[0].findAll("td", {"style":"padding-bottom:13px;color:#000000;"})
+
+    #最低5個まで登録する
+    word = "none"
+    for item in tdTag:
+        word = item.get_text()
+
+    #tableTag = soup.title
+    #table = tableTag.string
+    print(word)
